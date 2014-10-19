2c24d81946644b73057d81a61c61bd7eec3ad7cc
Guillaume <Guillaume@192.169.85.100>
2014-10-19 08:41:02 +0200

    Updating the unit test : using specific assertions for functions, objects; Small patch for Windows user when running vows : wildcard not well handled;

--- test/test.index.js
+++ test/test.index.js
@@ -5,45 +5,46 @@ var assert = require('assert'), vows = require('vows');
 var pgpool = require('../src/index');
 
 function assertPool(pool) {
-    assert.ok(pool, 'pool is defined');
-    assert.ok(pool.connection, 'pool has a `connection`  method');
-    assert.ok(pool.query, 'pool has a `query`  method');
-    assert.ok(pool.queryStream, 'pool has a `queryStream` method');
+    assert.isObject(pool, 'pool is defined');
+    assert.isFunction(pool.connection, 'pool has a `connection`  method');
+    assert.isFunction(pool.query, 'pool has a `query`  method');
+    assert.isFunction(pool.queryStream, 'pool has a `queryStream` method');
 }
 
+var URI = 'postgresql://localhost:5435/dbname';
 
 vows.describe('pg pool').addBatch({
-
+    
     'create a default unamed pool': {
         topic: function () {
-            return pgpool({url: 'postgresql://localhost:5435/dbname'}, {}, this.callback);
+            return pgpool({url: URI}, {}, this.callback);
         },
         'returns a pool object with a *connection* method ' : function (res) {
-            assert.ok(res.db.connection);
+            assert.isFunction(res.db.connection);
         },
         'returns a pool object with a *query* method ' : function (res) {
-            assert.ok(res.db.query);
+            assert.isFunction(res.db.query);
         },
         'returns a pool object with a *queryStream* method ' : function (res) {
-            assert.ok(res.db.queryStream);
+            assert.isFunction(res.db.queryStream);
         },
         'exports a *db* object to architect ' : function (res) {
-            assert.ok(res.db);
+            assert.isObject(res.db);
         }
     },
     'create a multi pools': {
         topic: function () {
             return pgpool({
                 first : {
-                    url: 'postgresql://localhost:5435/dbname'
+                    url: URI
                 },
                 second : {
-                    url: 'postgresql://localhost:5432/dbname'
+                    url: URI
                 }
             }, {}, this.callback);
         },
         'exports a *db* object to architect ' : function (res) {
-            assert.ok(res.db);
+            assert.isObject(res.db);
         },
         'returns a pool named *first* with a *connection* method ' : function (res) {
             assertPool(res.db.first);
@@ -52,17 +53,17 @@ vows.describe('pg pool').addBatch({
             assertPool(res.db.second);
         },
         'there is no *default* pool : *db.connection* is not avaliable ' : function (res) {
-            assert.ok(!res.db.connection);
+            assert.isUndefined(res.db.connection);
         }
     },
     'create a multi pools with a default': {
         topic: function () {
             return pgpool({
                 first : {
-                    url: 'postgresql://localhost:5435/dbname'
+                    url: URI
                 },
                 second : {
-                    url: 'postgresql://localhost:5432/dbname',
+                    url: URI,
                     'default':  true
                 }
             }, {}, this.callback);
